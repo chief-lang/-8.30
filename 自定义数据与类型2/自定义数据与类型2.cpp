eb2885cf32b04f3f0a805809caf2a512c9c33c38
chief-lang <2174853337@qq.com>
2024-08-30 13:24:32 +0800

    Add files via upload

---
+++ 自定义数据与类型2/自定义数据与类型2.cpp
@@ -0,0 +1,23 @@
+﻿//结构体的嵌套:一层一层
+#include<stdio.h>
+struct A
+{
+	int a;
+	struct
+	{
+		int b;
+	}c;
+};
+struct CC
+{
+	struct A a;
+
+};
+
+int main()
+{
+	A aaa;
+	aaa.c.b;
+	CC cc;
+	cc.a.c;
+}
